3ba0d95318ea226d57b49c9771c3a0b4cb64d08c
Tommy Muehle <tommy.muehle@gmail.com>
2016-06-24 12:12:00 +0200

    Fix message format in exception to prevent failure in PHP 5.5

--- src/Exception/FormatException.php
+++ src/Exception/FormatException.php
@@ -10,11 +10,21 @@ namespace TM\ErrorLogParser\Exception;
 class FormatException extends \Exception
 {
     /**
-     * @var string
+     * @param string     $message
+     * @param int        $code
+     * @param \Exception $previous
      */
-    public $message =
-        'The parser only supports the default Log-Format!' . PHP_EOL .
-        'If you think this is an error please refer to' . PHP_EOL .
-        'https://github.com/tommy-muehle/error-log-parser/issues' . PHP_EOL .
-        'and create an issue!';
+    public function __construct($message = null, $code = null, \Exception $previous = null)
+    {
+        $message = '
+            The parser only supports the default Log-Format!' . PHP_EOL . '
+            If you think this is an error please refer to' . PHP_EOL . '
+            https://github.com/tommy-muehle/error-log-parser/issues' . PHP_EOL . '
+            and create an issue!
+        ';
+
+        parent::__construct($message, $code, $previous);
+    }
+
+
 }
